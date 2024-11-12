0da6b05b34240e3423d588c10ef88551fe5bad48
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-11-12 21:07:21 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json'
+import css from './App.module.css'
 
 
 export const App = () => {
   return (
-    <div>
+    <div className={css.container}>
       <Profile
   username={user.username}
   tag={user.tag}
