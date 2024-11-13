2e7908d05f3592e52463fee5db9fbeafc589fe1f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-11-13 19:59:35 +0200

    stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,28 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
 import user from './Profile/user.json'
+import data from './Statistics/data.json'
+
 import css from './App.module.css'
 
 
 export const App = () => {
   return (
     <div className={css.container}>
+
       <Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-/>
+      />
+      
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+
     </div>
   );
 };
