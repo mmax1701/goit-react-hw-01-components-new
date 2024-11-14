a42fccd62fd26e90c9abbc8f6e455ce4ac675690
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-11-14 19:54:30 +0200

    finally

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,25 @@
-import {getRandomColor} from './Statistics.js'
-import css from './Statistics.module.css'
+import { getRandomColor } from './Statistics.js';
+import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
-    return (
-        <section className={css.statistics}>
-            {title && <h2 className={css.title}>{ title }</h2>}
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-            <ul className={css.statList}>
-                {stats.map(({id, label, percentage}) => {
-                    return <li className={css.item} style={{backgroundColor: getRandomColor()}} key={id}>
-                        <span className={css.label}>{ label }</span>
-                        <span className={css.percentage}>{ percentage }</span>
-                    </li>
-                })}
-            </ul> 
-        </section>
-    )
-}
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.item}
+              style={{ backgroundColor: getRandomColor() }}
+              key={id}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
