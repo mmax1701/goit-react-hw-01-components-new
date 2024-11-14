1034dd3738a8c08cdd0aee754c7ca20aacf6198a
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-11-14 19:35:09 +0200

    trans

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics.jsx';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from './Profile/user.json'
 import data from './Statistics/data.json'
 import friends from './FriendList/friends.json'
+import transactions from './TransactionHistory/transactions.json'
 
 import css from './App.module.css'
 
@@ -23,6 +25,7 @@ export const App = () => {
       
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
 
     </div>
   );
